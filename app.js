2847879af71a43b6f603af9ca746bb4b0a1910bf
Ben <ben@Bens-Mac-Pro.local>
2021-10-05 12:54:12 +0900

    #2.0

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+function onMouseMove(event){
+    console.log(event);
+}
+
+canvas.addEventListener("mousemove",onMouseMove);
